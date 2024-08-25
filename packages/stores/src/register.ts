56c0d2279b0f02f5772e327617219985465df157
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-25 15:31:28 +0800

    feat: add custom store for utilities

--- packages/stores/src/register.ts
+++ packages/stores/src/register.ts
@@ -1,20 +1,26 @@
 import { Plugin, SapphireClient, postLogin, preInitialization } from "@sapphire/framework";
+
 import { Services } from "./services/services";
 import type { ServicesStore } from "./services/services-store";
+import { Utilities } from "./utilities/utilities";
+import type { UtilitiesStore } from "./utilities/utilities-store";
 
 declare module "discord.js" {
     export interface Client {
         services: Services;
+        utilities: Utilities;
     }
 }
 
 declare module "@sapphire/pieces" {
     interface StoreRegistryEntries {
         services: ServicesStore;
+        utilities: UtilitiesStore;
     }
 
     interface Container {
         services: Services;
+        utilities: Utilities;
     }
 }
 
@@ -28,10 +34,14 @@ export class Stores extends Plugin {
     public static [preInitialization](this: SapphireClient): void {
         this.services = new Services();
         this.stores.register(this.services.store);
+
+        this.utilities = new Utilities();
+        this.stores.register(this.utilities.store);
     }
 
     public static [postLogin](this: SapphireClient): void {
         exposeStorePieces(this.services.store, this.services.exposePiece.bind(this.services));
+        exposeStorePieces(this.utilities.store, this.utilities.exposePiece.bind(this.utilities));
     }
 }
 
