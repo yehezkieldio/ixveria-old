40cfa8c8eee22131154875b02240c61af9aa8270
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-25 08:14:35 +0800

    initial: initial setup

---
+++ apps/discord-bot/tsup.config.ts
@@ -0,0 +1,13 @@
+import "../../packages/environment/env";
+
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/**/*.ts", "!src/**/*.d.ts", "!src/**/*.test.ts*"],
+    clean: true,
+    sourcemap: process.env.NODE_ENV !== "production",
+    minify: true,
+    skipNodeModulesBundle: true,
+    tsconfig: "tsconfig.json",
+    noExternal: ["@ixveria/environment"],
+});
