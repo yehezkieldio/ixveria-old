7811101439e7419a8f4d993b4746c4025018b540
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-27 17:43:34 +0800

    refactor: add jsdoc and encapsulate param into a context

--- apps/discord-bot/src/services/moderation.ts
+++ apps/discord-bot/src/services/moderation.ts
@@ -4,6 +4,12 @@ import type { Collection, Guild, GuildBan, GuildMember } from "discord.js";
 import { IxveriaIdentifiers } from "#lib/extensions/constants/identifiers";
 import type { ChatMessageContext } from "#lib/typings/message-type";
 
+interface ActionContext {
+    executor: GuildMember;
+    targetUser: GuildMember;
+    reason: string;
+}
+
 export class ModerationService extends Service {
     public constructor(context: Service.LoaderContext, options: Service.Options) {
         super(context, {
@@ -12,6 +18,11 @@ export class ModerationService extends Service {
         });
     }
 
+    /**
+     * Gets the bans in a guild.
+     * @param guild The guild to fetch the bans from.
+     * @returns The bans in the guild.
+     */
     private async fetchBans(guild: ChatMessageContext["guild"]): Promise<Collection<string, GuildBan>> {
         if (!guild) {
             throw new UserError({
@@ -22,6 +33,12 @@ export class ModerationService extends Service {
         return await guild.bans.fetch();
     }
 
+    /**
+     * Ensures the user is not banned.
+     * @param guild The guild where the action is being performed.
+     * @param targetUser The user to check.
+     * @returns Whether the user is not banned.
+     */
     private async ensureUserIsNotBanned(guild: ChatMessageContext["guild"], targetUser: GuildMember): Promise<void> {
         const bans = await this.fetchBans(guild);
 
@@ -33,6 +50,12 @@ export class ModerationService extends Service {
         }
     }
 
+    /**
+     * Ensures the user is banned.
+     * @param guild The guild where the action is being performed.
+     * @param targetUser The user to check.
+     * @returns Whether the user is banned.
+     */
     private async ensureUserIsBanned(guild: ChatMessageContext["guild"], targetUser: GuildMember): Promise<void> {
         const bans = await this.fetchBans(guild);
 
@@ -44,6 +67,15 @@ export class ModerationService extends Service {
         }
     }
 
+    /**
+     * Ensures the action to be performed is valid, by checking common conditions.
+     * @param guild The guild where the action is being performed.
+     * @param executor The user performing the action.
+     * @param targetUser The user the action is being performed on.
+     * @param action The action to be performed, either "kick", "ban" or "unban".
+     * @throws UserError
+     * @returns void
+     */
     private ensureActionIsValid(
         guild: Guild,
         executor: GuildMember,
@@ -93,6 +125,14 @@ export class ModerationService extends Service {
         }
     }
 
+    /**
+     * Validates the action to be performed.
+     * @param guild The guild where the action is being performed.
+     * @param executor The user performing the action.
+     * @param targetUser The user the action is being performed on.
+     * @param action The action to be performed, either "kick", "ban" or "unban".
+     * @returns Whether the action is valid.
+     */
     private async validateAction(
         guild: ChatMessageContext["guild"],
         executor: GuildMember,
@@ -112,38 +152,48 @@ export class ModerationService extends Service {
 
     /* -------------------------------------------------------------------------- */
 
-    public async kick(
-        guild: ChatMessageContext["guild"],
-        executor: GuildMember,
-        targetUser: GuildMember,
-        reason: string,
-    ): Promise<boolean> {
-        await this.validateAction(guild, executor, targetUser, "kick");
+    /**
+     * Performs a kick action on a user.
+     * @param guild The guild where the action is being performed.
+     * @param executor The user performing the action.
+     * @param targetUser The user to kick.
+     * @param reason The reason for the kick.
+     * @returns Whether the kick was successful.
+     */
+    public async kick(guild: ChatMessageContext["guild"], context: ActionContext): Promise<boolean> {
+        await this.validateAction(guild, context.executor, context.targetUser, "kick");
 
-        const kick = await targetUser.kick(reason);
+        const kick = await context.targetUser.kick(context.reason);
         if (kick) return true;
 
         return false;
     }
 
-    public async ban(
-        guild: ChatMessageContext["guild"],
-        executor: GuildMember,
-        targetUser: GuildMember,
-        reason: string,
-    ): Promise<boolean> {
-        await this.validateAction(guild, executor, targetUser, "ban");
+    /**
+     * Performs a ban action on a user.
+     * @param guild The guild where the action is being performed.
+     * @param context The context of the action, containing the executor, target user and reason.
+     * @returns Whether the ban was successful.
+     */
+    public async ban(guild: ChatMessageContext["guild"], context: ActionContext): Promise<boolean> {
+        await this.validateAction(guild, context.executor, context.targetUser, "ban");
 
-        const ban = await targetUser.ban({ reason });
+        const ban = await context.targetUser.ban({ reason: context.reason });
         if (ban) return true;
 
         return false;
     }
 
-    public async unban(guild: Guild, executor: GuildMember, targetUser: GuildMember, reason: string): Promise<boolean> {
-        await this.validateAction(guild, executor, targetUser, "unban");
+    /**
+     * Performs an unban action on a user.
+     * @param guild The guild where the action is being performed.
+     * @param context The context of the action, containing the executor, target user and reason.
+     * @returns Whether the unban was successful.
+     */
+    public async unban(guild: Guild, context: ActionContext): Promise<boolean> {
+        await this.validateAction(guild, context.executor, context.targetUser, "unban");
 
-        const unban = await guild.bans.remove(targetUser.id, reason);
+        const unban = await guild.bans.remove(context.targetUser.id, context.reason);
         if (unban) return true;
 
         return false;
