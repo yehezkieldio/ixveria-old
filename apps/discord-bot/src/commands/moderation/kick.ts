44a862cec433ca04a67fbee9213cfae9b233fd35
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-27 11:21:12 +0800

    refactor: improve kick command structure

--- apps/discord-bot/src/commands/moderation/kick.ts
+++ apps/discord-bot/src/commands/moderation/kick.ts
@@ -1,5 +1,12 @@
 import { type Args, CommandOptionsRunTypeEnum, type ResultType, UserError } from "@sapphire/framework";
-import { type GuildMember, type Message, PermissionFlagsBits, SlashCommandBuilder, type User } from "discord.js";
+import {
+    type Guild,
+    type GuildMember,
+    type Message,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+    type User,
+} from "discord.js";
 import { IxveriaCommand } from "#lib/extensions/command";
 import { IxveriaIdentifiers } from "#lib/extensions/constants/identifiers";
 import { IxveriaEmbedBuilder } from "#lib/extensions/embed-builder";
@@ -34,15 +41,15 @@ export class KickCommand extends IxveriaCommand {
     }
 
     #serverOnly = "This command can only be executed in a server.";
+    #defaultReason = "No reason provided.";
 
     /* -------------------------------------------------------------------------- */
 
     public async chatInputRun(interaction: IxveriaCommand.ChatInputCommandInteraction) {
-        const { moderation } = this.container.services;
         const { bot } = this.container.utilities;
 
         const user: User = interaction.options.getUser("user", true);
-        const reason: string = interaction.options.getString("reason") ?? "No reason provided.";
+        const reason: string = interaction.options.getString("reason") ?? this.#defaultReason;
 
         if (!interaction.guild) {
             throw new UserError({
@@ -54,45 +61,30 @@ export class KickCommand extends IxveriaCommand {
         const executor: GuildMember = await bot.getUserAsGuildMember(interaction.user.id, interaction.guild);
         const target: GuildMember = await bot.getUserAsGuildMember(user.id, interaction.guild);
 
-        await moderation.kick(interaction.guild, executor, target, reason);
+        const response: string = await this.kickUser(interaction.guild, executor, target, reason);
 
-        target.send({
-            embeds: [
-                new IxveriaEmbedBuilder()
-                    .setTheme("warning")
-                    .setDescription(`You have been kicked from ${interaction.guild.name} for reason: ${reason}`),
-            ],
-        });
-
-        return interaction.reply({
-            content: `Kicking ${user.tag} for reason: ${reason}`,
-        });
+        return interaction.reply(response);
     }
 
     public async messageRun(message: Message, args: Args): Promise<Message> {
-        const { moderation } = this.container.services;
         const { bot } = this.container.utilities;
 
         const userArgument: ResultType<User> = await args.restResult("user");
-
         if (userArgument.isErr()) {
             throw new UserError({
                 identifier: IxveriaIdentifiers.ArgsMissing,
                 message: "You didn't provide any user to kick, how am I supposed to do that?",
             });
         }
+        const user: User = userArgument.unwrap();
 
         const reasonArgument: ResultType<string> = await args.restResult("string");
-
+        let reason: string;
         if (reasonArgument.isErr()) {
-            throw new UserError({
-                identifier: IxveriaIdentifiers.ArgsMissing,
-                message: "You didn't provide any reason for kicking the user.",
-            });
+            reason = this.#defaultReason;
+        } else {
+            reason = reasonArgument.unwrap();
         }
-
-        const user: User = userArgument.unwrap();
-        const reason: string = reasonArgument.unwrap();
 
         if (!message.guild) {
             throw new UserError({
@@ -104,16 +96,25 @@ export class KickCommand extends IxveriaCommand {
         const executor: GuildMember = await bot.getUserAsGuildMember(message.author.id, message.guild);
         const target: GuildMember = await bot.getUserAsGuildMember(user.id, message.guild);
 
-        await moderation.kick(message.guild, executor, target, reason);
+        const response: string = await this.kickUser(message.guild, executor, target, reason);
+
+        return message.reply(response);
+    }
+
+    /* -------------------------------------------------------------------------- */
+
+    private async kickUser(guild: Guild, executor: GuildMember, target: GuildMember, reason: string) {
+        const { moderation } = this.container.services;
+        await moderation.kick(guild, executor, target, reason);
 
         target.send({
             embeds: [
                 new IxveriaEmbedBuilder()
                     .setTheme("warning")
-                    .setDescription(`You have been kicked from ${message.guild.name} for reason: ${reason}`),
+                    .setDescription(`You have been kicked from ${guild.name} for reason: ${reason}`),
             ],
         });
 
-        return message.channel.send(`Kicking ${user.tag} for reason: ${reason}`);
+        return `Kicking ${target.user.tag} for reason: ${reason}`;
     }
 }
