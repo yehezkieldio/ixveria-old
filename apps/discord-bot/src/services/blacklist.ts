12128af1a737b78c2c8699af8df7bb0e21ebffc7
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-28 16:29:03 +0800

    chore: jsdoc

--- apps/discord-bot/src/services/blacklist.ts
+++ apps/discord-bot/src/services/blacklist.ts
@@ -12,6 +12,10 @@ export class BlacklistService extends Service {
         });
     }
 
+    /**
+     * Gets all the blacklisted servers.
+     * @returns The blacklisted servers.
+     */
     public async getServers(): Promise<SelectBlacklistedEntities[]> {
         const servers: SelectBlacklistedEntities[] = await database
             .select()
@@ -22,6 +26,10 @@ export class BlacklistService extends Service {
         return servers;
     }
 
+    /**
+     * Gets all the blacklisted users.
+     * @returns The blacklisted users.
+     */
     public async getUsers(): Promise<SelectBlacklistedEntities[]> {
         const users: SelectBlacklistedEntities[] = await database
             .select()
@@ -32,6 +40,11 @@ export class BlacklistService extends Service {
         return users;
     }
 
+    /**
+     * Ensures the user is blacklisted.
+     * @param userId The user to check.
+     * @returns Whether the user is blacklisted.
+     */
     public async ensureUserIsBlacklisted(userId: string): Promise<boolean> {
         const user: SelectBlacklistedEntities[] = await database
             .select()
@@ -41,6 +54,11 @@ export class BlacklistService extends Service {
         return !!user;
     }
 
+    /**
+     * Ensures the server is blacklisted.
+     * @param guildId The server to check.
+     * @returns Whether the server is blacklisted.
+     */
     public async ensureServerIsBlacklisted(guildId: string): Promise<boolean> {
         const server: SelectBlacklistedEntities[] = await database
             .select()
@@ -50,6 +68,12 @@ export class BlacklistService extends Service {
         return !!server;
     }
 
+    /**
+     * Creates a blacklist entity.
+     * @param entityId The entity to blacklist.
+     * @param entityType The type of entity to blacklist, either "user" or "guild".
+     * @returns Whether the entity was blacklisted.
+     */
     public async createBlacklist(entityId: string, entityType: "user" | "guild"): Promise<boolean> {
         const entity = await database
             .insert(blacklistEntities)
@@ -63,6 +87,11 @@ export class BlacklistService extends Service {
         return true;
     }
 
+    /**
+     * Deletes a blacklist entity.
+     * @param entityId The entity to remove from the blacklist.
+     * @returns Whether the entity was removed from the blacklist.
+     */
     public async removeBlacklist(entityId: string): Promise<boolean> {
         const entity = await database
             .delete(blacklistEntities)
